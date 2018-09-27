c0184256f7cdf78907c90574edf45c72e0a6086b
alphonseb <alphonse.bouy@gmail.com>
2018-09-27 20:05:44 +0200

    :lipstick: homepage

--- templates/pages/homepage.php
+++ templates/pages/homepage.php
@@ -1,17 +1,18 @@
 <div class="homepage is-displayed section" data-select='1'>
     <video src="dist/img/homevideofinal.webm" loop muted autoplay></video>
     <img class="homepage__shoe mouse-parallax" src="dist/img/shoes/shoe3.png" alt="Freestate shoe">
-    <div class="arrow mouse-parallax-slow">
 
-        <p>Back us on kickstarter !</p>
-        <img class="arrow_img" src="dist/img/arrow.png" alt="Arrow">
-    </div>
     <div class="mouse-parallax-slow homepage__content">
         <h1 class="">Freestate.</h1>
         <h3>One pair of shoes. Infinite destinations.</h3>
         <div class="homepage__content__links">
             <a id="travel" href="#" class="mouse-magnet"><p>Begin the journey here</p></a>
-            <div>
+            <div class="buttons">
+                <div class="arrow">
+
+                    <p>Back us on kickstarter !</p>
+                    <img class="arrow_img" src="dist/img/arrow.png" alt="Arrow">
+                </div>
                 <a class="kickstarter" href="https://www.kickstarter.com/projects/79677955/freestate-travel-shoes-have-never-looked-this-good" title="Support us on kickstarter">
                     <img src="dist/img/kickstarter_white.png" alt="kickstarter">
 
