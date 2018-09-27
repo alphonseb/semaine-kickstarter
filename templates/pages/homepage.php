27e1becdb2e262636c602a7c21fd5675d380e4f1
alphonseb <alphonse.bouy@gmail.com>
2018-09-27 19:06:28 +0200

    :lipstick: homepage

--- templates/pages/homepage.php
+++ templates/pages/homepage.php
@@ -1,18 +1,30 @@
 <div class="homepage is-current section" data-rank='1'>
     <video src="dist/img/homevideofinal.webm" loop muted autoplay></video>
     <img class="homepage__shoe mouse-parallax" src="dist/img/shoes/shoe3.png" alt="Freestate shoe">
+    <div class="arrow">
+
+        <p>Back us on kickstarter !</p>
+        <img class="arrow_img" src="dist/img/arrow.png" alt="Arrow">
+    </div>
     <div class="mouse-parallax-slow homepage__content">
         <h1 class="">Freestate.</h1>
         <h3>One pair of shoes. Infinite destinations.</h3>
         <div class="homepage__content__links">
             <a id="travel" href="#" class="mouse-magnet"><p>Begin the journey here</p></a>
             <div>
-                <a href="https://www.kickstarter.com/projects/79677955/freestate-travel-shoes-have-never-looked-this-good" title="Support us on kickstarter">
+                <a class="kickstarter" href="https://www.kickstarter.com/projects/79677955/freestate-travel-shoes-have-never-looked-this-good" title="Support us on kickstarter">
                     <img src="dist/img/kickstarter_white.png" alt="kickstarter">
+
                 </a>
-                <a href="#" title="Share the experience">
-                    <img src="dist/img/social.png" alt="Share Icon">
-                </a>
+                <div class="socials">
+
+                    <a class="share" href="#" title="Share the experience">
+                        <img src="dist/img/social.png" alt="Share Icon">
+                    </a>
+                    <a class="network1" href="#"><img src="dist/img/facebook.png" alt="Facebook Icon"></a>
+                    <a class="network2" href="#"><img src="dist/img/twitter.png" alt="Twitter Icon"></a>
+                    <a class="network3" href="#"><img src="dist/img/instagram.png" alt="Instagram Icon"></a>
+                </div>
             </div>
 
         </div>
