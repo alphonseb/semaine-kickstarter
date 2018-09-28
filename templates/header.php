515274dc83aa84b045e093cdd41614d0cbae2c14
alphonseb <alphonse.bouy@gmail.com>
2018-09-28 11:55:15 +0200

    header

--- templates/header.php
+++ templates/header.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.2/fullpage.min.css">
+    <title>Freestate</title>
     <link rel="stylesheet" href="dist/css/app.css">
 </head>
 <body>
